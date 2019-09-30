0c3ba8b0005993cfaaa2a6d194f8f4fc7ff45955
HasheDev <hashedev@gmail.com>
2019-09-30 09:07:37 -0300

    adding command tpa

--- src/not/domain/Home.java
+++ src/not/domain/Home.java
@@ -7,11 +7,13 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import java.util.HashMap;
 public class Home extends JavaPlugin{
     String plname = ChatColor.BLUE + "[" + "Essentials" + ChatColor.DARK_GREEN + " Hsb " + ChatColor.BLUE + "]"; //name this plugin
     String plversion = ChatColor.DARK_GREEN + "1"+ChatColor.BLUE+"."+ChatColor.DARK_GREEN+"0";
     String pldev = "Hashing";
 	public ConsoleCommandSender warn = Bukkit.getConsoleSender(); //warns
+	HashMap<Player, Player> playertpa = new HashMap<Player, Player>(); //HashMap
 	
 	@Override
 	public void onEnable() {
@@ -32,6 +34,33 @@ public class Home extends JavaPlugin{
 				p.sendMessage(plversion);
 				p.sendMessage(ChatColor.BLUE+ " [Download] > " + ChatColor.DARK_GREEN + "https://github.com/HasheDev/EssentialsHsb/releases/tag/1.0");
 			}
+		 else if(command.getName().equalsIgnoreCase("tpa")) {
+			if(args.length == 1) {
+				Player target = p.getServer().getPlayer(args[0]);			  
+				if(target != null) {
+					if(!target.getName().equals(p.getName()))
+					{
+						playertpa.put(target, p);
+					p.sendMessage("Você pediu tpa para " + target.getName());
+					target.sendMessage(p.getName()+ " esta le pedindo teleporte use /tpaccept ou /tpadeny");
+					}
+
+				}
+			}
+		}else if(command.getName().equalsIgnoreCase("tpaccept")) {
+			if(playertpa.get(p) != null) {
+				playertpa.get(p).teleport(p);
+				p.sendMessage("Você aceitou o tpa de " + playertpa.get(p).getName());
+				playertpa.get(p).sendMessage("Seu tpa foi aceito " + playertpa.get(p).getName());
+				playertpa.put(p, null);
+			}
+		}else if(command.getName().equalsIgnoreCase("tpadeny")) {
+			if(playertpa.get(p) != null) {
+				p.sendMessage("Você cancelou o pedido de teleporte de " + playertpa.get(p).getName());
+				playertpa.get(p).sendMessage("Seu tpa não foi aceito " + playertpa.get(p).getName());
+				playertpa.put(p, null);
+			}
+		}
 		return false;
 	}
 }
