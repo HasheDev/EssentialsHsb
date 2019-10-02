fe1e0805967f2d3a7860a4973dc4fb2e9235c433
HasheDev <hashedev@gmail.com>
2019-10-02 15:45:06 -0300

    adding command fly

--- src/not/domain/Home.java
+++ src/not/domain/Home.java
@@ -6,6 +6,7 @@ import org.bukkit.GameMode;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.command.defaults.KickCommand;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import java.util.HashMap;
@@ -29,7 +30,12 @@ public class Home extends JavaPlugin{
 		warn.sendMessage(ChatColor.BLUE+"* "+ ChatColor.RED + "desligado com sucesso");
 	}
 	@Override
+	@SuppressWarnings("deprecation")
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if (!(sender instanceof Player) ) {
+			sender.sendMessage("Só jogadores podem usar o comando");
+			return true;
+		}else {
 		 Player p = (Player) sender;
 		 if(command.getName().equalsIgnoreCase("essentialshsb")) {
 				p.sendMessage(plname);
@@ -44,6 +50,24 @@ public class Home extends JavaPlugin{
 					p.sendMessage(ChatColor.DARK_RED+"/tpa: "+cmdcolor+"fazer pedido de teleporte");
 				}else {
 					p.sendMessage("Você não tem a permissão: "+permission + " para usar este comando");
+				}
+			}else if(command.getName().equalsIgnoreCase("kick")) {
+				if(args.length == 0) {
+					p.sendMessage("Use: /kick player");
+				}else if(args.length > 0) {
+					Player player = (Player) sender;
+					Player target = player.getServer().getPlayer(args[0]);
+					if(target != null) {
+					if(args.length == 1) {
+					   if(args[0].equalsIgnoreCase(args[0])) {
+							target.kickPlayer("Confidencial");
+					   }
+					}else if(args.length == 2) {
+						target.kickPlayer(args[1]);
+					  }
+					}else {
+						p.sendMessage("Player não esta conectado");
+					}
 				}
 			}
 		 else if(command.getName().equalsIgnoreCase("gm") | command.getName().equalsIgnoreCase("gamemode")) {
@@ -125,6 +149,8 @@ public class Home extends JavaPlugin{
 				playertpa.put(p, null);
 			}
 		}
+		}
 		return false;
 	}
+		
 }
